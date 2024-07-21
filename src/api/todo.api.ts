95ab74a7c9d6ccf54aa344eb3bcc433e8c41bd4e
DaliborD45 <dalibordetko@gmail.com>
2024-07-21 11:29:50 +0200

    feat: implemented api calls

---
+++ src/api/todo.api.ts
@@ -0,0 +1,84 @@
+import { TodoT } from "@/types/todo.type"
+import { TodoListT } from "@/types/todoList.type"
+import { revalidatePath } from "next/cache"
+import { BASE_API_URL } from "./baseApiUrl"
+
+export const getTodos = async (todolistId: TodoListT["id"]) => {
+  try {
+    const response = await fetch(`${BASE_API_URL}/todolist/${todolistId}/todos`)
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const addTodo = async (todolistId: TodoListT["id"], todo: TodoT) => {
+  try {
+    const response = await fetch(
+      `${BASE_API_URL}/todolist/${todolistId}/todos`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
+      }
+    )
+    revalidatePath("/")
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateTodo = async (todolistId: TodoListT["id"], todo: TodoT) => {
+  try {
+    const response = await fetch(
+      `${BASE_API_URL}/todolist/${todolistId}/todos/${todo.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
+      }
+    )
+
+    revalidatePath("/")
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const deleteTodo = async (
+  todolistId: TodoListT["id"],
+  todoId: TodoT["id"]
+) => {
+  try {
+    const response = await fetch(
+      `${BASE_API_URL}/todolist/${todolistId}/todos/${todoId}`,
+      {
+        method: "DELETE",
+      }
+    )
+    revalidatePath("/")
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getTodo = async (
+  todolistId: TodoListT["id"],
+  todoId: TodoT["id"]
+) => {
+  try {
+    const response = await fetch(
+      `${BASE_API_URL}/todolist/${todolistId}/todos/${todoId}`
+    )
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ src/api/todoList.api.ts
@@ -0,0 +1,30 @@
+import { TodoListT } from "@/types/todoList.type"
+import { revalidatePath } from "next/cache"
+import { BASE_API_URL } from "./baseApiUrl"
+
+export const getTodoLists = async () => {
+  try {
+    const response = await fetch(`${BASE_API_URL}/todolist`, {
+      cache: "no-cache",
+    })
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const addTodoList = async (todoList: TodoListT) => {
+  try {
+    const response = await fetch(`${BASE_API_URL}/todolist`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todoList),
+    })
+    revalidatePath("/")
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
